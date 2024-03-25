e802b55e52e02ab5c065cc1ab6b395fd732c39ed
Nejanna <nejanna288.gmail.com>
2024-03-25 20:52:20 +0200

    new

--- server/logit_test.py
+++ server/logit_test.py
@@ -1,10 +1,16 @@
 import requests
+import unittest
 
-def test_login():
-    url = 'http://your-backend-url/login'
-    data = {'email': 'test@example.com', 'password': 'password'}  # Замініть на реальні дані
-    response = requests.post(url, json=data)
-    
-    assert response.status_code == 200
-    assert 'token' in response.json()
-    assert 'user' in response.json()
+class TestLogin(unittest.TestCase):
+    def test_successful_login(self):
+        url = 'http://localhost:3000/' 
+        data = {'email': 'tryryryrt@gmail.com', 'password': '9078'} 
+        response = requests.post(url, json=data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('token', response.json())
+        self.assertIn('user', response.json())
+
+if __name__ == '__main__':
+    unittest.main()
+
